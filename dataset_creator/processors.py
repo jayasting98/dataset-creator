0b7f28161904e6c8c3b0b6bf232201e110d7c8b7
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-02-24 00:50:03 +0800

    Add abstract Processor class

---
+++ dataset_creator/processors.py
@@ -0,0 +1,28 @@
+import abc
+from typing import Any
+from typing import Generic
+from typing import Iterable
+from typing import Self
+from typing import TypeVar
+
+from dataset_creator.loaders import Loader
+from dataset_creator.savers import Saver
+
+
+_T = TypeVar('_T')
+_U = TypeVar('_U')
+
+
+class Processor(abc.ABC, Generic[_T, _U]):
+    @abc.abstractmethod
+    def __init__(
+        self: Self,
+        loader: Loader[_T],
+        saver: Saver[_U],
+        config: dict[str, Any],
+    ) -> None:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def load(self: Self) -> Iterable[_T]:
+        raise NotImplementedError()
