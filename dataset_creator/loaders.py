86b77cf7d72484d73de9f7d786e079afe1079c20
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-02-24 01:13:04 +0800

    Add HuggingFaceLoader

--- dataset_creator/loaders.py
+++ dataset_creator/loaders.py
@@ -5,6 +5,8 @@ from typing import Iterable
 from typing import Self
 from typing import TypeVar
 
+import datasets
+
 
 _T = TypeVar('_T')
 
@@ -17,3 +19,12 @@ class Loader(abc.ABC, Generic[_T]):
     @abc.abstractmethod
     def load(self: Self) -> Iterable[_T]:
         raise NotImplementedError()
+
+
+class HuggingFaceLoader(Loader[_T]):
+    def __init__(self: Self, config: dict[str, Any]) -> None:
+        self._config = dict(streaming=True, **config)
+
+    def load(self: Self) -> Iterable[_T]:
+        dataset = datasets.load_dataset(**self._config)
+        return dataset
