c25224ffda6d61d416b66077b06b5d9f233858bf
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-02-24 23:33:27 +0800

    Use datasets.Dataset in HuggingFaceLoaderTest

--- unit_tests/test_loaders.py
+++ unit_tests/test_loaders.py
@@ -1,5 +1,8 @@
+from typing import Any
 import unittest
 from unittest import mock
+
+import datasets
 
 from dataset_creator import loaders
 
@@ -7,10 +10,16 @@ from dataset_creator import loaders
 class HuggingFaceLoaderTest(unittest.TestCase):
     def test_load__streaming_not_configured__loads_via_stream(self):
         config = {'path': 'path'}
-        loader = loaders.HuggingFaceLoader[str](config)
+        loader = loaders.HuggingFaceLoader[dict[str, Any]](config)
+        def generator():
+            yield {'message': 'Hello'}
+            yield {'message': 'World!'}
+        dataset = datasets.Dataset.from_generator(generator)
         with mock.patch('datasets.load_dataset') as mock_load_dataset:
-            mock_load_dataset.return_value = ['Hello', 'World!']
+            mock_load_dataset.return_value = dataset
             iterator = loader.load()
         mock_load_dataset.assert_called_once_with(path='path', streaming=True)
-        self.assertEqual('Hello', next(iterator))
-        self.assertEqual('World!', next(iterator))
+        self.assertEqual({'message': 'Hello'}, next(iterator))
+        self.assertEqual({'message': 'World!'}, next(iterator))
+        with self.assertRaises(StopIteration):
+            next(iterator)
