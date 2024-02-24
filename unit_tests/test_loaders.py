a8c5bed4dc5aff805841ccdeb92109034d1d13b9
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-02-24 18:28:56 +0800

    Add unit testing for HuggingFaceLoader

---
+++ unit_tests/test_loaders.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest import mock
+
+from dataset_creator import loaders
+
+
+class HuggingFaceLoaderTest(unittest.TestCase):
+    def test_load__streaming_not_configured__loads_via_stream(self):
+        config = {'path': 'path'}
+        loader = loaders.HuggingFaceLoader[str](config)
+        with mock.patch('datasets.load_dataset') as mock_load_dataset:
+            mock_load_dataset.return_value = ['Hello', 'World!']
+            dataset = loader.load()
+        mock_load_dataset.assert_called_once_with(path='path', streaming=True)
+        self.assertEqual('Hello', dataset[0])
+        self.assertEqual('World!', dataset[1])
