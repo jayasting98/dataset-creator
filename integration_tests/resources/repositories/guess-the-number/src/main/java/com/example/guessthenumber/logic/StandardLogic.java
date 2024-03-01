138d8df6215fadbf330897322eda3284124fb6ee
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-02 02:32:03 +0800

    Add StandardLogic

---
+++ integration_tests/resources/repositories/guess-the-number/src/main/java/com/example/guessthenumber/logic/StandardLogic.java
@@ -0,0 +1,41 @@
+package com.example.guessthenumber.logic;
+
+import java.util.Random;
+
+public class StandardLogic implements Logic {
+    static final int LOWER_BOUND = 1;
+    static final int UPPER_BOUND = 100;
+    static final int MAX_NUM_GUESSES = 6;
+
+    private GameState state;
+    private int numGuessesTaken;
+    private int numberToGuess;
+
+    public StandardLogic(Random random) {
+        state = GameState.START;
+        numGuessesTaken = 0;
+        numberToGuess = random.nextInt(UPPER_BOUND) + 1;
+    }
+
+    @Override
+    public boolean isAbleToGuess() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAbleToGuess'");
+    }
+
+    @Override
+    public void process(int guessedNumber) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    }
+
+    @Override
+    public GameState getState() {
+        return state;
+    }
+
+    @Override
+    public int getNumberToGuess() {
+        return numberToGuess;
+    }
+}
---
+++ integration_tests/resources/repositories/guess-the-number/src/test/java/com/example/guessthenumber/logic/StandardLogicTest.java
@@ -0,0 +1,3 @@
+package com.example.guessthenumber.logic;
+
+public class StandardLogicTest {}
