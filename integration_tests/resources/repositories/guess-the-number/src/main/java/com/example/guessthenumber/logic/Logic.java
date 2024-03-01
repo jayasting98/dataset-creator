86b8a937ef7fe2dc719fb603009485a2c5c8b924
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-02 00:47:05 +0800

    Add CommandLineUi and Logic

---
+++ integration_tests/resources/repositories/guess-the-number/src/main/java/com/example/guessthenumber/logic/Logic.java
@@ -0,0 +1,3 @@
+package com.example.guessthenumber.logic;
+
+public interface Logic {}
---
+++ integration_tests/resources/repositories/guess-the-number/src/main/java/com/example/guessthenumber/ui/CommandLineUi.java
@@ -0,0 +1,20 @@
+package com.example.guessthenumber.ui;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+
+import com.example.guessthenumber.logic.Logic;
+
+public class CommandLineUi implements UserInterface {
+    private BufferedReader inputReader;
+    private PrintStream outputWriter;
+    private Logic logic;
+
+    public CommandLineUi(BufferedReader inputReader, PrintStream outputWriter, Logic logic) {
+        this.inputReader = inputReader;
+        this.outputWriter = outputWriter;
+        this.logic = logic;
+    }
+
+    public void run() {}
+}
