e72630f32df4f48807bfb80a931bf275171fe175
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-02 03:13:32 +0800

    Implement main method

--- integration_tests/resources/repositories/guess-the-number/src/main/java/com/example/guessthenumber/App.java
+++ integration_tests/resources/repositories/guess-the-number/src/main/java/com/example/guessthenumber/App.java
@@ -1,7 +1,27 @@
 package com.example.guessthenumber;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Random;
+
+import com.example.guessthenumber.logic.Logic;
+import com.example.guessthenumber.logic.StandardLogic;
+import com.example.guessthenumber.ui.CommandLineUi;
+import com.example.guessthenumber.ui.UserInterface;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        UserInterface ui = constructUserInterface();
+        ui.run();
+    }
+
+    private static UserInterface constructUserInterface() {
+        Random random = new Random();
+        Logic logic = new StandardLogic(random);
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+        PrintStream outputWriter = System.out;
+        UserInterface ui = new CommandLineUi(inputReader, outputWriter, logic);
+        return ui;
     }
 }
