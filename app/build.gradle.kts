350fcdd54d738db3592b8a28c5acef4e1add9061
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-02 22:42:30 +0800

    Rename Code Cov Server package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.App"
+    mainClass = "com.jayasting98.codecovserver.App"
 }
 
 tasks.named<Test>("test") {
