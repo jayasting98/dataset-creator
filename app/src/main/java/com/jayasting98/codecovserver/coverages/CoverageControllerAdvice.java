0980f9ccb9fad4edbdbb4e379211cc2c80cd8fbd
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-05 16:13:19 +0800

    Add error logging

--- app/src/main/java/com/jayasting98/codecovserver/coverages/CoverageControllerAdvice.java
+++ app/src/main/java/com/jayasting98/codecovserver/coverages/CoverageControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.jayasting98.codecovserver.coverages;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,10 +12,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class CoverageControllerAdvice {
     static final String UNEXPECTED_ERROR_MESSAGE = "An unexpected error has occurred.";
 
+    final Logger logger = LoggerFactory.getLogger(getClass());
+
     @ResponseBody
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     String handleException(Exception e) {
+        logger.error(UNEXPECTED_ERROR_MESSAGE, e);
+        Throwable t = e.getCause();
+        while (t != null) {
+            logger.error("Cause of " + e.toString(), t);
+            t = t.getCause();
+        }
         return UNEXPECTED_ERROR_MESSAGE;
     }
 }
