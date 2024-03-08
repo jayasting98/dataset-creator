9539739b9084c29cfab19a31c1d8fa0d820898cc
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-09 00:42:11 +0800

    Add CodeCovApi

--- dataset_creator/coverages.py
+++ dataset_creator/coverages.py
@@ -1,4 +1,8 @@
+import os
+from typing import Self
 from typing import TypedDict
+
+import requests
 
 
 class CreateCoverageRequestData(TypedDict):
@@ -12,3 +16,21 @@ class CreateCoverageRequestData(TypedDict):
 
 class Coverage(TypedDict):
     coveredLineNumbers: list[int]
+
+
+class CodeCovApi:
+    def __init__(
+        self: Self,
+        session: requests.Session,
+        base_url: str,
+    ) -> None:
+        self._session = session
+        self._base_url = base_url
+
+    def create_coverage(
+        self: Self,
+        request_data: CreateCoverageRequestData,
+    ) -> requests.Response:
+        url = os.path.join(self._base_url, 'coverages')
+        response = self._session.post(url, json=request_data)
+        return response
