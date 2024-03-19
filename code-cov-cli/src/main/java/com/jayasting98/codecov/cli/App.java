321e84a67d05b2f21d13f51b59040b281fcca00e
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-20 03:05:19 +0800

    Implement code-cov-cli main method

--- code-cov-cli/src/main/java/com/jayasting98/codecov/cli/App.java
+++ code-cov-cli/src/main/java/com/jayasting98/codecov/cli/App.java
@@ -1,6 +1,55 @@
 package com.jayasting98.codecov.cli;
 
+import java.io.PrintStream;
+import java.util.List;
+import java.util.function.Function;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayasting98.codecov.cli.ui.CommandLineUi;
+import com.jayasting98.codecov.cli.ui.UserInterface;
+import com.jayasting98.codecov.data.coverages.Coverage;
+import com.jayasting98.codecov.data.coverages.CreateCoverageRequestData;
+import com.jayasting98.codecov.utilities.CoverageAnalyzer;
+
 public class App {
     public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Function<String, CreateCoverageRequestData> deserializer = jsonString -> {
+            CreateCoverageRequestData requestData;
+            try {
+                requestData = objectMapper.readValue(jsonString, CreateCoverageRequestData.class);
+            } catch (JsonProcessingException e) {
+                throw new IllegalArgumentException(
+                    "JSON string must represent valid instance of CreateCoverageRequestData.", e);
+            }
+            return requestData;
+        };
+        Function<CreateCoverageRequestData, Coverage> processor = requestData -> {
+            CoverageAnalyzer coverageAnalyzer = new CoverageAnalyzer(
+                requestData.getClasspathPathnames(), requestData.getFocalClasspath(),
+                requestData.getFocalClassName(), requestData.getTestClassName(),
+                requestData.getTestMethodName());
+            List<Integer> coveredLineNumbers;
+            try {
+                coveredLineNumbers = coverageAnalyzer.findCoveredLineNumbers();
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Covered line numbers could not be found.", e);
+            }
+            Coverage coverage = new Coverage(coveredLineNumbers);
+            return coverage;
+        };
+        Function<Coverage, String> serializer = coverage -> {
+            String jsonString;
+            try {
+                jsonString = objectMapper.writeValueAsString(coverage);
+            } catch (JsonProcessingException e) {
+                throw new IllegalArgumentException("Coverage had the wrong format.", e);
+            }
+            return jsonString;
+        };
+        PrintStream outputWriter = System.out;
+        UserInterface ui = new CommandLineUi<>(deserializer, processor, serializer, outputWriter);
+        ui.run(args);
     }
 }
--- code-cov-cli/src/test/java/com/jayasting98/codecov/cli/AppTest.java
+++ code-cov-cli/src/test/java/com/jayasting98/codecov/cli/AppTest.java
@@ -1,10 +1,60 @@
 package com.jayasting98.codecov.cli;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.io.PrintStream;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayasting98.codecov.data.coverages.CreateCoverageRequestData;
 
 class AppTest {
     @Test
-    void testMain_typicalCase_doesNothing() {
-        App.main(new String[0]);
+    void testMain_typicalCase_printsCoverageJsonString() throws JsonProcessingException {
+        String projectDirPathname = Paths.get("").toAbsolutePath().getParent().toString();
+        String repositoryDirPathname = Paths.get(projectDirPathname, "integration_tests",
+            "resources", "repositories", "maven", "guess-the-number", "").toString();
+        String mavenDirPathname =
+            Paths.get(System.getProperty("user.home"), ".m2", "repository").toString();
+        String junitJarPathname =
+            Paths.get(mavenDirPathname, "junit", "junit", "4.11", "junit-4.11.jar").toString();
+        String hamcrestCoreJarPathname = Paths.get(mavenDirPathname, "org", "hamcrest",
+            "hamcrest-core", "1.3", "hamcrest-core-1.3.jar").toString();
+        String mockitoCoreJarPathname = Paths.get(mavenDirPathname, "org", "mockito",
+            "mockito-core", "3.12.4", "mockito-core-3.12.4.jar").toString();
+        String byteBuddyJarPathname = Paths.get(mavenDirPathname, "net", "bytebuddy", "byte-buddy",
+            "1.11.13", "byte-buddy-1.11.13.jar").toString();
+        String byteBuddyAgentJarPathname = Paths.get(mavenDirPathname, "net", "bytebuddy",
+            "byte-buddy-agent", "1.11.13", "byte-buddy-agent-1.11.13.jar").toString();
+        String objenesisJarPathname = Paths.get(mavenDirPathname, "org", "objenesis", "objenesis",
+            "3.2", "objenesis-3.2.jar").toString();
+        String focalClasspath =
+            Paths.get(repositoryDirPathname, "target", "classes", "").toString();
+        String testClasspath =
+            Paths.get(repositoryDirPathname, "target", "test-classes", "").toString();
+        List<String> classpathPathnames = Arrays.asList(focalClasspath, testClasspath,
+            junitJarPathname, hamcrestCoreJarPathname, mockitoCoreJarPathname, byteBuddyJarPathname,
+            byteBuddyAgentJarPathname, objenesisJarPathname);
+            String focalClassName = "com.example.guessthenumber.ui.CommandLineUi";
+            String testClassName = "com.example.guessthenumber.ui.CommandLineUiTest";
+            String testMethodName = "testParseGuess";
+        CreateCoverageRequestData requestData = new CreateCoverageRequestData(classpathPathnames,
+            focalClasspath, focalClassName, testClassName, testMethodName);
+        ObjectMapper mapper = new ObjectMapper();
+        String requestDataJsonString = mapper.writeValueAsString(requestData);
+        String[] args = new String[] {requestDataJsonString};
+        PrintStream originalOut = System.out;
+        PrintStream mockOut = mock(PrintStream.class);
+        System.setOut(mockOut);
+        App.main(args);
+        verify(mockOut)
+            .println("{\"coveredLineNumbers\":[24,25,26,27,28,67,68,69,70,71,72,73,88,89]}");
+        System.setOut(originalOut);
     }
 }
