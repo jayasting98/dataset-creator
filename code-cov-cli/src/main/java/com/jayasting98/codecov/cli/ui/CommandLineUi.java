7baf75c436ae34c0a3db3b313af8e39c71aa0dfe
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-20 00:53:02 +0800

    Add CommandLineUi

---
+++ code-cov-cli/src/main/java/com/jayasting98/codecov/cli/ui/CommandLineUi.java
@@ -0,0 +1,10 @@
+package com.jayasting98.codecov.cli.ui;
+
+public class CommandLineUi implements UserInterface {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+}
